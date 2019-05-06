3acba0e638d0912960a6c44bc0b30953737059b3
Luca Ban <mesqueeb@gmail.com>
2019-05-07 08:36:02 +0900

    update dependencies ⚜️

--- test/index.js
+++ test/index.js
@@ -1,8 +1,9 @@
 import test from 'ava'
 import flatten from '../dist/index.cjs'
+import copy from 'copy-anything'
 
 test('flatten', t => {
-  let res, target
+  let res, target, original
   target = {
     a: 1,
     b: {
@@ -10,27 +11,35 @@ test('flatten', t => {
       e: 'yes'
     }
   }
+  original = copy(target)
   res = flatten(target)
   t.deepEqual(res, {
     'a': 1,
     'b.c.d': true,
     'b.e': 'yes'
   })
+  t.deepEqual(target, original)
 })
 test('flatten special objects', t => {
-  let res, target
+  let res, target, original
   target = {}
+  original = copy(target)
   res = flatten(target)
   t.deepEqual(res, {})
+  t.deepEqual(target, original)
   const date = new Date()
   target = {a: date}
+  original = copy(target)
   res = flatten(target)
   t.deepEqual(res, {a: date})
+  t.deepEqual(target, original)
 })
 
 test('flatten arrays', t => {
-  let res, target
+  let res, target, original
   target = [1, 2, ['a', 'b', ['y', 'z']], 3, [{a: {b: 1}}]]
+  original = copy(target)
   res = flatten(target)
   t.deepEqual(res, [1, 2, 'a', 'b', 'y', 'z', 3, {a: {b: 1}}])
+  t.deepEqual(target, original)
 })
