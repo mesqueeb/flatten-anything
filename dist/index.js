e23010da727fe9145a887e513350dce13e505d11
Luca Ban <mesqueeb@gmail.com>
2023-05-23 11:20:50 +0900

    fix: exports

---
+++ dist/index.js
@@ -0,0 +1,51 @@
+import { isArray, isPlainObject, isNumber } from 'is-what';
+import { omit } from 'filter-anything';
+
+function retrievePaths(object, path, result, untilDepth) {
+  if (!isPlainObject(object) || !Object.keys(object).length || object.methodName === "FieldValue.serverTimestamp") {
+    if (!path)
+      return object;
+    result[path] = object;
+    return result;
+  }
+  if (isNumber(untilDepth))
+    untilDepth--;
+  return Object.keys(object).reduce((carry, key) => {
+    const pathUntilNow = path ? path + "." : "";
+    const newPath = pathUntilNow + key;
+    const extra = untilDepth === -1 ? { [newPath]: object[key] } : retrievePaths(object[key], newPath, result, untilDepth);
+    return Object.assign(carry, extra);
+  }, {});
+}
+function flattenObject(object, untilDepth) {
+  const result = {};
+  return retrievePaths(object, null, result, untilDepth);
+}
+function flattenArray(array) {
+  return array.reduce((carry, item) => {
+    return isArray(item) ? [...carry, ...flattenArray(item)] : [...carry, item];
+  }, []);
+}
+function flattenObjectProps(object, props = []) {
+  const flatObject = props.reduce((carry, propPath) => {
+    const firstPropKey = propPath.split(".")[0];
+    const target = { [firstPropKey]: object[firstPropKey] };
+    const untilDepth = propPath.split(".").length - 1 || void 0;
+    const flatPart = flattenObject(target, untilDepth);
+    const flatPartFiltered = Object.entries(flatPart).reduce((carry2, [key, value]) => {
+      if (!key.startsWith(propPath))
+        return carry2;
+      carry2[key] = value;
+      return carry2;
+    }, {});
+    return { ...carry, ...flatPartFiltered };
+  }, {});
+  const omittedProps = props;
+  const objectWithoutFlatProps = omit(object, omittedProps);
+  return { ...objectWithoutFlatProps, ...flatObject };
+}
+function flatten(objectOrArray, untilDepth) {
+  return isArray(objectOrArray) ? flattenArray(objectOrArray) : flattenObject(objectOrArray, untilDepth);
+}
+
+export { flatten, flattenArray, flattenObject, flattenObjectProps };
