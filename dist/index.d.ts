258850dc8520781d89d2ca6af3f751b47958b3f0
Luca Ban <mesqueeb@users.noreply.github.com>
2026-09-26 10:54:49 +1200

    chore: rebuild dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,10 +1,11 @@
 /**
  * Flattens an object from `{a: {b: {c: 'd'}}}` to `{'a.b.c': 'd'}`
  *
+ * @param object The object to flatten
+ * @param [number] How deep you want to flatten. 1 for flattening only the first nested prop, and
+ *   keeping deeper objects as is.
+ * @returns The flattened object
  * @export
- * @param object the object to flatten
- * @param [number] how deep you want to flatten. 1 for flattening only the first nested prop, and keeping deeper objects as is.
- * @returns the flattened object
  */
 export declare function flattenObject(object: {
     [key in string]: any;
@@ -14,18 +15,19 @@ export declare function flattenObject(object: {
 /**
  * Flattens an array from `[1, ['a', ['z']], 2]` to `[1, 'a', 'z', 2]`
  *
+ * @param array The array to flatten
+ * @returns The flattened array
  * @export
- * @param array the array to flatten
- * @returns the flattened array
  */
 export declare function flattenArray(array: any[]): any[];
 /**
  * Flattens certain props of an object.
  *
+ * @param object The object to flatten Eg. `{a: {subA: 1}, b: {subB: 1}}`
+ * @param [props=[]] The prop names you want to flatten. Eg. `['a']` will return `{'a.subA': 1, b:
+ *   {subB: 1}}`. Default is `[]`
+ * @returns The flattened object
  * @export
- * @param object the object to flatten Eg. `{a: {subA: 1}, b: {subB: 1}}`
- * @param [props=[]] the prop names you want to flatten. Eg. `['a']` will return `{'a.subA': 1, b: {subB: 1}}`
- * @returns the flattened object
  */
 export declare function flattenObjectProps(object: {
     [key in string]: any;
@@ -33,14 +35,14 @@ export declare function flattenObjectProps(object: {
     [key in string]: any;
 };
 /**
- * Flattens an object or array.
- * Object example: `{a: {b: {c: 'd'}}}` to `{'a.b.c': 'd'}`
- * Array example: `[1, ['a', ['z']], 2]` to `[1, 'a', 'z', 2]`
+ * Flattens an object or array. Object example: `{a: {b: {c: 'd'}}}` to `{'a.b.c': 'd'}` Array
+ * example: `[1, ['a', ['z']], 2]` to `[1, 'a', 'z', 2]`
  *
+ * @param objectOrArray The payload to flatten
+ * @param [number] How deep you want to flatten. (currently only works with objects) 1 for
+ *   flattening only the first nested prop, and keeping deeper objects as is.
+ * @returns The flattened result
  * @export
- * @param objectOrArray the payload to flatten
- * @param [number] how deep you want to flatten. (currently only works with objects) 1 for flattening only the first nested prop, and keeping deeper objects as is.
- * @returns the flattened result
  */
 export declare function flatten(objectOrArray: {
     [key in string]: any;
--- dist/index.js
+++ dist/index.js
@@ -24,10 +24,11 @@ function retrievePaths(object, path, result, untilDepth) {
 /**
  * Flattens an object from `{a: {b: {c: 'd'}}}` to `{'a.b.c': 'd'}`
  *
+ * @param object The object to flatten
+ * @param [number] How deep you want to flatten. 1 for flattening only the first nested prop, and
+ *   keeping deeper objects as is.
+ * @returns The flattened object
  * @export
- * @param object the object to flatten
- * @param [number] how deep you want to flatten. 1 for flattening only the first nested prop, and keeping deeper objects as is.
- * @returns the flattened object
  */
 export function flattenObject(object, untilDepth) {
     const result = {};
@@ -36,9 +37,9 @@ export function flattenObject(object, untilDepth) {
 /**
  * Flattens an array from `[1, ['a', ['z']], 2]` to `[1, 'a', 'z', 2]`
  *
+ * @param array The array to flatten
+ * @returns The flattened array
  * @export
- * @param array the array to flatten
- * @returns the flattened array
  */
 export function flattenArray(array) {
     return array.reduce((carry, item) => {
@@ -48,10 +49,11 @@ export function flattenArray(array) {
 /**
  * Flattens certain props of an object.
  *
+ * @param object The object to flatten Eg. `{a: {subA: 1}, b: {subB: 1}}`
+ * @param [props=[]] The prop names you want to flatten. Eg. `['a']` will return `{'a.subA': 1, b:
+ *   {subB: 1}}`. Default is `[]`
+ * @returns The flattened object
  * @export
- * @param object the object to flatten Eg. `{a: {subA: 1}, b: {subB: 1}}`
- * @param [props=[]] the prop names you want to flatten. Eg. `['a']` will return `{'a.subA': 1, b: {subB: 1}}`
- * @returns the flattened object
  */
 export function flattenObjectProps(object, props = []) {
     const flatObject = props.reduce((carry, propPath) => {
@@ -73,14 +75,14 @@ export function flattenObjectProps(object, props = []) {
     return { ...objectWithoutFlatProps, ...flatObject };
 }
 /**
- * Flattens an object or array.
- * Object example: `{a: {b: {c: 'd'}}}` to `{'a.b.c': 'd'}`
- * Array example: `[1, ['a', ['z']], 2]` to `[1, 'a', 'z', 2]`
+ * Flattens an object or array. Object example: `{a: {b: {c: 'd'}}}` to `{'a.b.c': 'd'}` Array
+ * example: `[1, ['a', ['z']], 2]` to `[1, 'a', 'z', 2]`
  *
+ * @param objectOrArray The payload to flatten
+ * @param [number] How deep you want to flatten. (currently only works with objects) 1 for
+ *   flattening only the first nested prop, and keeping deeper objects as is.
+ * @returns The flattened result
  * @export
- * @param objectOrArray the payload to flatten
- * @param [number] how deep you want to flatten. (currently only works with objects) 1 for flattening only the first nested prop, and keeping deeper objects as is.
- * @returns the flattened result
  */
 export function flatten(objectOrArray, untilDepth) {
     return isArray(objectOrArray)
